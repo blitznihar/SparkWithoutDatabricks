0ae9f33ab5305ce7bf2a05d72fde813a65b6b9b3
Nihar Malali <blitznihar@gmail.com>
2024-12-07 18:58:53 -0600

    Refactor test_reuseGenerateData to streamline notebook execution and file handling

--- tests/test_reuseGenerateData.py
+++ tests/test_reuseGenerateData.py
@@ -1,26 +1,21 @@
 import os
 from testbook import testbook
 
-# from reuseGenerateData import HealthData
-# os.path.exists("reuseGenerateData.ipynb")
+
 @testbook('reuseGenerateData.ipynb', execute=True)
 def test_reuseGenerateData(tb):
-        HealthData = tb.get("HealthData")
-    #with open("reuseGenerateData.ipynb") as f:
-        #nb = nbformat.read(f, as_version=4)
-        environment = "test"
-        outputdir = "tests/data"
-        filename = f"{outputdir}/health_data_{environment}.parquet"
-        os.makedirs(outputdir, exist_ok=True)
+    HealthData = tb.get("HealthData")
+    environment = "test"
+    outputdir = "tests/data"
+    filename = f"{outputdir}/health_data_{environment}.parquet"
+    os.makedirs(outputdir, exist_ok=True)
 
-        if os.path.exists(filename):
-            os.remove(filename)
+    if os.path.exists(filename):
+        os.remove(filename)
+    print(f"Running notebook with environment {environment}")
 
-        print(f"Running notebook with environment {environment}")
-
-        healthData = HealthData(environment, outputdir)  # type: ignore
-        healthData.generate_data()
-        assert os.path.exists(filename), f"{filename} has not been created"
-        if os.path.exists(filename):
-            os.remove(filename)
-            
+    healthData = HealthData(environment, outputdir)  # type: ignore
+    healthData.generate_data()
+    assert os.path.exists(filename), f"{filename} has not been created"
+    if os.path.exists(filename):
+        os.remove(filename)
