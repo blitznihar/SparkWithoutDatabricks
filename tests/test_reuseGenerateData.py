3db746f3eda090326df2b6c062a9d61147bb07e6
Nihar Malali <blitznihar@gmail.com>
2024-12-07 19:07:01 -0600

    Remove unnecessary blank lines in Jupyter Notebooks and update variable names for consistency

--- tests/test_reuseGenerateData.py
+++ tests/test_reuseGenerateData.py
@@ -1,20 +1,19 @@
 import os
 from testbook import testbook
 
-
-@testbook('reuseGenerateData.ipynb', execute=True)
+@testbook("reuseGenerateData.ipynb", execute=True)
 def test_reuseGenerateData(tb):
     HealthData = tb.get("HealthData")
     environment = "test"
-    outputdir = "tests/data"
-    filename = f"{outputdir}/health_data_{environment}.parquet"
-    os.makedirs(outputdir, exist_ok=True)
+    outdir = "tests/data"
+    filename = f"{outdir}/health_data_{environment}.parquet"
+    os.makedirs(outdir, exist_ok=True)
 
     if os.path.exists(filename):
         os.remove(filename)
     print(f"Running notebook with environment {environment}")
 
-    healthData = HealthData(environment, outputdir)  # type: ignore
+    healthData = HealthData(environment, outdir)  # type: ignore
     healthData.generate_data()
     assert os.path.exists(filename), f"{filename} has not been created"
     if os.path.exists(filename):
